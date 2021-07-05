a1c896358cbf3e3f8b29f35205776d8c23dcde43
Mayhem924 <fenyatop@gmail.com>
2021-07-06 00:23:02 +0300

    7. Reverse Integer

--- ReverseInteger/Program.cs
+++ ReverseInteger/Program.cs
@@ -6,7 +6,53 @@ namespace ReverseInteger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"123 -> {ReverseInt(123)}");     // 321
+            Console.WriteLine($"-123 -> {ReverseInt(-123)}");   // -321
+            Console.WriteLine($"120 -> {ReverseInt(120)}");     // 21
+
+            Console.ReadLine();
+        }
+
+        static int ReverseInt(int x)
+        {
+            int tempInt;
+
+            try
+            {
+                tempInt = Math.Abs(x);
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+
+            var tempStr = tempInt.ToString();
+            var chars = tempStr.ToCharArray();
+
+            Array.Reverse(chars);
+            tempStr = String.Empty;
+
+            foreach (var item in chars)
+            {
+                tempStr += item;
+            }
+
+            try
+            {
+                tempInt = Convert.ToInt32(tempStr);
+            }
+            catch (OverflowException)
+            {
+                tempInt = 0;
+            }
+
+
+            if (x < 0)
+            {
+                tempInt = -tempInt;
+            }
+
+            return tempInt;
         }
     }
 }
