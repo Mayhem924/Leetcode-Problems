e9d92dc7351d1fbff69f024eb631412251232c86
Mayhem924 <fenyatop@gmail.com>
2021-07-06 00:37:10 +0300

    20. Valid Parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static Dictionary<char, char> brackets = new Dictionary<char, char>();
+
+        static void Main(string[] args)
+        {
+            brackets.Add('{', '}');
+            brackets.Add('(', ')');
+            brackets.Add('[', ']');
+
+            Console.WriteLine("()[]{} -> " + IsValid("()[]{}"));            // true
+            Console.WriteLine("([)] -> " + IsValid("([)]"));                // false
+            Console.WriteLine("(){([])}{} -> " + IsValid("(){([])}{}"));    // true
+
+            Console.ReadLine();
+        }
+
+        static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var current in s)
+            {
+                brackets.TryGetValue(current, out char closeBracket);
+
+                if (brackets.ContainsValue(closeBracket))
+                {
+                    stack.Push(current);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    if (current != brackets[stack.Pop()])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
