5d68df88be3a98ea57e6628f6435dc2e9ee63ef2
Никита Ходяков <fenyatop@gmail.com>
2021-08-06 16:31:00 +0300

    28. Implement strStr()

---
+++ Implement strStr()/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Implement_strStr__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"hello - ll -> {StrStr("hello", "ll")}");    // 2
+            Console.WriteLine($"aaaa - bba -> {StrStr("aaaa", "bba")}");    // -1
+            Console.WriteLine($"'' - '' -> {StrStr("", "")}");              // 0
+
+            Console.ReadLine();
+        }
+
+        static int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
