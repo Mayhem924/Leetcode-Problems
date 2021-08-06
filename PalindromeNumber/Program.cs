9818e805bbecb950cc730209844b3c592be7ea55
Никита Ходяков <fenyatop@gmail.com>
2021-08-06 16:12:47 +0300

    9. Palindrome Number

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"121 -> {IsPalindrome(121)}");   // true
+            Console.WriteLine($"-121 -> {IsPalindrome(-121)}"); // false
+            Console.WriteLine($"-101 -> {IsPalindrome(-101)}"); // false
+
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            var charSpan = x.ToString().ToCharArray().AsSpan();
+            charSpan.Reverse();
+
+            return charSpan.SequenceEqual(x.ToString().AsSpan());
+        }
+    }
+}
